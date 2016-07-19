84bbfa82b46a0870b6e2318df81cca30097f8935
Mike <psycorpse@gmail.com>
2016-07-19 08:02:01 -0500

    Initial Commit

---
+++ source/Game1.cpp
@@ -0,0 +1,102 @@
+#include "Game1.h"
+
+Game1::Game1() {
+	
+}
+
+void Game1::Initialize() {
+	GX_ClearVtxDesc();
+    	GX_SetVtxDesc(GX_VA_POS, GX_DIRECT);
+    	GX_SetVtxDesc(GX_VA_CLR0, GX_DIRECT);
+	GX_SetVtxDesc(GX_VA_TEX0, GX_DIRECT);
+	
+	GX_SetVtxAttrFmt(GX_VTXFMT0, GX_VA_POS, GX_POS_XYZ, GX_F32, 0);
+    	GX_SetVtxAttrFmt(GX_VTXFMT0, GX_VA_CLR0, GX_CLR_RGBA, GX_RGBA8, 0);
+	GX_SetVtxAttrFmt(GX_VTXFMT0, GX_VA_TEX0, GX_TEX_ST, GX_F32, 0);
+	
+	GX_SetNumChans(1);
+	GX_SetNumTexGens(1);
+	GX_SetTevOrder (GX_TEVSTAGE0, GX_TEXCOORD0,GX_TEXMAP0, GX_COLOR0A0);
+        GX_SetTevOp (GX_TEVSTAGE0, GX_MODULATE);        
+        GX_SetTexCoordGen(GX_TEXCOORD0, GX_TG_MTX2x4, GX_TG_TEX0, GX_IDENTITY);
+
+	camera = new Camera( Game::gxrmode, (guVector){0,0, -5}, (guVector){0,1,0}, (guVector){0,0,0});
+}
+
+void Game1::LoadContent() {
+
+	// Load the textures
+	TPLFile texturesTPL;
+
+	GXTexObj _background;
+	TPL_OpenTPLFromMemory(&texturesTPL, (void *)textures_tpl, textures_tpl_size);
+	TPL_GetTexture(&texturesTPL, background, &_background);
+	textures.push_back(_background);
+}
+
+void Game1::Update() {
+	Game::Update();
+
+	// Update goes here
+	camera->Update();
+
+}
+
+
+void Game1::Draw() {
+	// Draw goes here
+	camera->Setup3D();
+	
+	DrawCube(*camera->GetView());
+
+	//int i = 0;
+	//for(i = 0; i < (int)platforms.size(); i++) {
+//		platforms[i]->Draw(*camera->GetView());
+//	}
+	
+	Game::Draw();
+}
+
+void Game1::DrawCube(Mtx44 view) {
+	Mtx44 model, modelview;
+	guMtxIdentity(model);
+	guMtxTransApply(model, model, 0,  0, 0);
+	guMtxConcat(model, view, modelview);
+	GX_LoadPosMtxImm(modelview, GX_PNMTX0);
+	GX_LoadTexObj(&textures[0], GX_TEXMAP0);
+	
+	float width, height, depth;
+
+	width = 27.0f;
+	height = 12.5f;
+	depth = 25.0f;
+	GX_Begin(GX_QUADS, GX_VTXFMT0, 4);
+		GX_Position3f32(-width, -height, depth);
+		GX_Color4u8(255, 0, 0, 255);
+		GX_TexCoord2f32(0,1);  
+
+		GX_Position3f32(width, -height, depth);
+		GX_Color4u8(255, 0, 0, 255);
+		GX_TexCoord2f32(1,1);
+
+		GX_Position3f32(width, height, depth);
+		GX_Color4u8(255, 0, 0, 255);
+		GX_TexCoord2f32(1,0);
+
+		GX_Position3f32(-width, height, depth);
+		GX_Color4u8(255, 0, 0, 255);
+		GX_TexCoord2f32(0,0);
+	GX_End();
+}
+
+Camera* Game1::GetCamera() {
+	return this->camera;
+}
+
+void Game1::Run() {
+
+	while(Game::IsRunning()) {
+		Update();
+		Draw();
+	}
+}
---
+++ source/Game1.h
@@ -0,0 +1,34 @@
+#ifndef __GAME1_H__
+#define __GAME1_H__
+
+#include "Game.h"
+#include "Camera.h"
+
+#include "textures_tpl.h"
+#include "textures.h"
+
+#include "Platforms.h"
+#include "Player.h"
+#include "Enemy.h"
+
+class Game1 : public Game {
+	public:
+		Game1();
+		void Initialize();
+		void LoadContent();
+		void Run();
+		void Update();
+		void Draw();
+		Camera* GetCamera();
+		void DrawCube(Mtx44);
+
+	private:
+		Camera * camera;
+		std::vector<GXTexObj> textures;
+		//std::vector<Platform*> platforms;
+
+
+	protected:
+};
+
+#endif
